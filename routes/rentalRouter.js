2e8193e03a613a1729388339b91f42574c8bbcaa
Shiyuan Miao <shiyuanm000@gmail.com>
2024-12-14 16:40:42 -0800

    dynamically fetch all rental sources

--- routes/rentalRouter.js
+++ routes/rentalRouter.js
@@ -6,9 +6,8 @@ const rentalRouter = Router();
 const db = new Firestore();
 
 const formatPrice = (price) => {
-  console.log(price);
   if (!price) return null;
-  return parseInt(price.replace(/[$,]/g, ""), 10) || null;
+  return parseInt(price.replace(/[$,]/g, ""), 10) || 0;
 };
 
 rentalRouter.get("/:source", async (req, res) => {
@@ -37,21 +36,19 @@ rentalRouter.get("/:source", async (req, res) => {
 
 rentalRouter.get("/", async (req, res) => {
   try {
-    const sources = [SOURCES.CRAIGSLIST, SOURCES.VANPEOPLE];
+    const sourcesSnapshot = await db
+      .collection("rental_listings")
+      .listDocuments();
     const allListings = {};
 
     await Promise.all(
-      sources.map(async (source) => {
-        const snapshot = await db
-
-          .collection("rental_listings")
-          .doc(source)
-          .collection("listings")
-          .get();
+      sourcesSnapshot.map(async (sourceDoc) => {
+        const source = sourceDoc.id;
+        const listingsSnapshot = await sourceDoc.collection("listings").get();
 
         allListings[source] = [];
 
-        snapshot.forEach((doc) => {
+        listingsSnapshot.forEach((doc) => {
           const data = doc.data();
           allListings[source].push({
             id: doc.id,
